f1934764cf357b54022581a71adc70af38405dac
BS <famersbs@gmail.com>
2019-01-31 08:52:48 -0800

    Reduce chart days when card has not enought data

--- src/containers/list/card/body/view.js
+++ src/containers/list/card/body/view.js
@@ -7,37 +7,76 @@ import { colorMap } from '../../../../type'
 
 import { SUMMARY_DAY } from '../../../../modules/list'
 
-const label = new Array(SUMMARY_DAY).fill(0).map((v,i) => `${SUMMARY_DAY - i} days ago` )
+const labels = new Array(SUMMARY_DAY).fill(0).map((v,i) => `${SUMMARY_DAY - i} days ago` )
 
 // 이 계산 부분을 Redux쪽으로 옮겨야 될듯, 이 계산을 해당 카드 타입에 따른 계산 모듈 쪽으로 옮겨서 처리 하
 // 도록 바꿔야 할듯...
 const getData = (logs) =>  {
-  let data = new Array(SUMMARY_DAY).fill(0)
-  let now = moment()
+  let data = new Array(SUMMARY_DAY).fill(null)
+  let now = moment().hours(0)
+  let oldest_target_date = now
+  let current_labels = labels;
 
   logs.forEach(l => {
-    const diffrence = now.diff(moment.unix(l.target_date), 'days')
-    if(diffrence >= SUMMARY_DAY || diffrence < 0) return // ignore paster than SUMMARY_DAY or future data
-    data[diffrence] = l.log.amount
+    const target_date = moment.unix(l.target_date).hours(0)
+    const diffrence = now.diff(target_date, 'days')
+
+    // For reduce chart days
+    if(oldest_target_date.isAfter(target_date)) oldest_target_date = target_date
+
+    // ignore paster than SUMMARY_DAY or future data
+    if(diffrence >= SUMMARY_DAY || diffrence < 0) return
+
+    // sum of the same day count
+    data[diffrence] = Number(l.log.amount)
   })
 
+  // If some day has no amount, it should be use previous amount
+  let previousAmount = 0
+  data = data.map(d => {
+    if(d == null) return previousAmount
+    else {
+      previousAmount = d
+    }
+    return d
+  })
+
+  // For reduce chart days
+  const otd_distance = now.diff(oldest_target_date, 'days') + 1
+  if (otd_distance < SUMMARY_DAY ) {
+    current_labels = labels.slice(SUMMARY_DAY - otd_distance)
+    data = data.slice(0, otd_distance)
+  }
+
   return {
-    labels: label,
+    labels: current_labels,
     datasets: [
       {
         borderWidth: 2,
-        data: data.reverse(),  // 2주 데이터
+        data: data.reverse(),
         ...colorMap.good,
+
+        pointDotRadius: 1,
+        pointColor: "rgba(87, 167, 134, 1)",
+        pointStrokeColor: "rgba(255, 255, 255, 0)",
+        pointHighlightFill: "rgba(87, 167, 134, 0.7)",
+        pointHighlightStroke: "rgba(87, 167, 134, 1)",
       }
     ],
   }
 }
+
 
 const chartOption = (goal) => ({
     responsive: true,
     maintainAspectRatio: false,
     legend: {
         display: false
+    },
+    tooltips: {
+      callbacks: {
+        title: (tooltipItem, chart) => ([])
+      }
     },
     scales: {
         xAxes: [{display: false}],
--- src/containers/list/card/workout/view.js
+++ src/containers/list/card/workout/view.js
@@ -7,27 +7,50 @@ import { colorMap } from '../../../../type'
 
 import { SUMMARY_DAY } from '../../../../modules/list'
 
-const label = new Array(SUMMARY_DAY).fill(0).map((v,i) => `${SUMMARY_DAY - i} days ago` )
+const labels = new Array(SUMMARY_DAY).fill(0).map((v,i) => `${SUMMARY_DAY - i} days ago` )
 
 // 이 계산 부분을 Redux쪽으로 옮겨야 될듯, 이 계산을 해당 카드 타입에 따른 계산 모듈 쪽으로 옮겨서 처리 하
 // 도록 바꿔야 할듯...
 const getData = (logs) =>  {
   let data = new Array(SUMMARY_DAY).fill(0)
-  let now = moment()
+  let now = moment().hours(0)
+  let oldest_target_date = now
+  let current_labels = labels;
 
   logs.forEach(l => {
-    const diffrence = now.diff(moment.unix(l.target_date), 'days')
-    if(diffrence >= SUMMARY_DAY || diffrence < 0) return // ignore paster than SUMMARY_DAY or future data
-    data[diffrence] = l.log.amount
+    const target_date = moment.unix(l.target_date).hours(0)
+    const diffrence = now.diff(target_date, 'days')
+
+    // For reduce chart days
+    if(oldest_target_date.isAfter(target_date)) oldest_target_date = target_date
+
+    // ignore paster than SUMMARY_DAY or future data
+    if(diffrence >= SUMMARY_DAY || diffrence < 0) return
+
+    // sum of the same day count
+    data[diffrence] += Number(l.log.amount)
   })
 
+  // For reduce chart days
+  const otd_distance = now.diff(oldest_target_date, 'days') + 1
+  if (otd_distance < SUMMARY_DAY ) {
+    current_labels = labels.slice(SUMMARY_DAY - otd_distance)
+    data = data.slice(0, otd_distance)
+  }
+
   return {
-    labels: label,
+    labels: current_labels,
     datasets: [
       {
         borderWidth: 2,
-        data: data.reverse(),  // 2주 데이터
+        data: data.reverse(),
         ...colorMap.good,
+
+        pointDotRadius: 1,
+        pointColor: "rgba(87, 167, 134, 1)",
+        pointStrokeColor: "rgba(255, 255, 255, 0)",
+        pointHighlightFill: "rgba(87, 167, 134, 0.7)",
+        pointHighlightStroke: "rgba(87, 167, 134, 1)",
       }
     ],
   }
@@ -35,21 +58,28 @@ const getData = (logs) =>  {
 
 const chartOption = (goal) => ({
     responsive: true,
-    maintainAspectRatio: false,
+    maintainAspectRatio: true,
+    pointDotRadius: 1,
+    pointDotStrokeWidth: 4,
     legend: {
         display: false
+    },
+    tooltips: {
+      callbacks: {
+        title: (tooltipItem, chart) => ([])
+      }
     },
     scales: {
         xAxes: [{display: false}],
         yAxes: [{display: false}]
     },
     elements: {
-        point: {
+        /*point: {
             radius: 0,
             hitRadius: 10,
             hoverRadius: 4,
             hoverBorderWidth: 3
-        },
+        },*/
     },
     horizontalLine: [{
         y: goal,
@@ -62,11 +92,12 @@ const Workout = (props) => {
 
   return (
     <div className="chart-container"
-        style={{zIndex:"0", position: "relative", height:"30px", width:"100%"}}>
+        style={{zIndex:"0", position: "relative", height:"100px", width:"100%"}}>
         <Line
-            data={getData(logs)}
-            options={chartOption(setting.baseline)}
-            />
+          height={100}
+          data={getData(logs)}
+          options={chartOption(setting.baseline)}
+        />
     </div>
   )
 }
