61be46821686f0c1ea30582c9655af8683aeecc3
BS <famersbs@gmail.com>
2019-01-30 21:14:58 -0800

    Fixed bug when added book card

--- src/containers/list/card/book/view.js
+++ src/containers/list/card/book/view.js
@@ -8,7 +8,10 @@ import {colorMap} from '../../../../type'
 const Book = (props) => {
     const { setting, summary, updated_at } = props
     const differnecy = moment().diff(moment.unix(updated_at), 'days')
-    const percentage = Math.ceil((summary.progress / setting.amount ) * 100)
+    let percentage = 0
+    if (summary != null && summary.progress != null) {
+      percentage = Math.ceil((summary.progress / setting.amount ) * 100)
+    }
 
     let color = colorMap.good
     if(differnecy === 1) color = colorMap.normal
